4cefe4b1561f425d71d736c4aeaa6131fed42ad6
Tanuj Mittal <tanuj.183@gmail.com>
2015-11-25 20:39:38 -0800

    Add LRU cache for data blocks

--- src/disk_emulator.c
+++ src/disk_emulator.c
@@ -2,9 +2,13 @@
 #include "mkfs.h"
 #include "common.h"
 #include "inode_table.h"
+#include "uthash.h"
 // in memory emulation of file system
 // #define IN_MEMORY_FS
 
+#define MAX_DATA_BLOCK_CACHE_SIZE 1024
+
+struct data_block_entry *data_block_cache = NULL;
 
 #ifdef IN_MEMORY_FS
 
@@ -38,6 +42,7 @@ void free_disk_emulator(void) { // just for in memory simulation; substitue code
 		free(block_data);
 		block_data = NULL;
 		purge_inode_table();
+		purge_data_block_cache();
 	}
 }
 
@@ -94,6 +99,7 @@ void free_disk_emulator(void){
 
 	disk_created = -1;
 	purge_inode_table();
+	purge_data_block_cache();
 	if(close(disk_store) == 0) {
 		LOGD("disk store successfully closed");
 	} else {
@@ -106,6 +112,11 @@ int read_block(big_int block_id, void * target) {
 	if(block_id >= NUM_BLOCKS) {
 		fprintf(stderr, "cannot read block id outside range\n");
 		return -1;
+	}
+
+	// Check the cache
+	if(get_data_block_from_cache(block_id, target) == 0) {
+		return 0;
 	}
 
 	off_t seek_pos = block_id * BLOCK_SIZE;
@@ -123,6 +134,8 @@ int read_block(big_int block_id, void * target) {
 	if((big_int) read_bytes != BLOCK_SIZE) {
 		LOGD("read returned less than expected bytes: %" PRId64, read_bytes);
 	}
+
+	put_data_block_in_cache(block_id, target);
 	return 0;
 }
 
@@ -159,10 +172,73 @@ int write_block(big_int block_id, void * buffer, size_t buffer_size) {
 	if((big_int) write_bytes != BLOCK_SIZE) {
 		fprintf(stderr, "write returned less than expected bytes (%" PRId64 ")\n", write_bytes);
 	}
+
+	put_data_block_in_cache(block_id, buffer);
 	return 0;
 }
 
 #endif
 
+int get_data_block_from_cache(big_int data_block_id, void *data_block) {
+    struct data_block_entry *data_block_entry;
 
+    HASH_FIND(hh, data_block_cache, &data_block_id, sizeof(big_int), data_block_entry);
+    if (data_block_entry) {
+        LOGD("Cache hit for data block %" PRIu64, data_block_id);
+        // Found in cache. Move it to front.
+        HASH_DEL(data_block_cache, data_block_entry);
+        HASH_ADD(hh, data_block_cache, data_block_id, sizeof(big_int), data_block_entry);
+
+        memcpy(data_block, data_block_entry->data_block, BLOCK_SIZE);
+        return 0;
+    }
+    LOGD("Cache miss for data block %" PRIu64, data_block_id);
+    return -1;
+}
+
+int put_data_block_in_cache(big_int data_block_id, void *data_block) {
+    if(data_block == NULL) {
+        return -1;
+    }
+
+    struct data_block_entry *data_block_entry, *tmp;
+
+    data_block_entry = (struct data_block_entry*) malloc(sizeof(struct data_block_entry));
+    data_block_entry->data_block_id = data_block_id;
+    data_block_entry->data_block = malloc(BLOCK_SIZE);;
+    memcpy(data_block_entry->data_block, data_block, BLOCK_SIZE);
+
+    LOGD("Adding to cache data block %" PRIu64, data_block_id);
+    HASH_REPLACE(hh, data_block_cache, data_block_id, sizeof(big_int), data_block_entry, tmp);
+    free_data_block_entry(tmp);
+    tmp = NULL;
+    
+    // prune the cache to MAX_DATA_BLOCK_CACHE_SIZE
+    if (HASH_COUNT(data_block_cache) >= MAX_DATA_BLOCK_CACHE_SIZE) {
+        HASH_ITER(hh, data_block_cache, data_block_entry, tmp) {
+            // prune the first entry (loop is based on insertion order so this deletes the oldest item)
+            LOGD("Removing from cache data block %" PRIu64, data_block_entry->data_block_id);
+            HASH_DEL(data_block_cache, data_block_entry);
+            free_data_block_entry(data_block_entry);
+            break;
+        }
+    }
+    return 0;
+}
+
+void purge_data_block_cache(void) {
+    struct data_block_entry *data_block_entry, *tmp;
+
+    HASH_ITER(hh, data_block_cache, data_block_entry, tmp) {
+        HASH_DEL(data_block_cache, data_block_entry);
+        free_data_block_entry(data_block_entry);
+    }
+}
+
+void free_data_block_entry(struct data_block_entry *data_block_entry) {
+    if(data_block_entry != NULL) {
+        free(data_block_entry->data_block);
+        free(data_block_entry);
+    }
+}
 
--- src/disk_emulator.h
+++ src/disk_emulator.h
@@ -2,11 +2,25 @@
 #define DISK_EMULATOR
 
 #include "common.h"
+#include "uthash.h"
+
+struct data_block_entry {
+    big_int data_block_id;
+    void *data_block;
+    UT_hash_handle hh;
+};
 
 int init_disk_emulator(void);
 void free_disk_emulator(void);
 
 int read_block(big_int block_id, void* target);
 int write_block(big_int block_id, void* buffer, size_t buffer_size);
+
+void purge_data_block_cache(void);
+
+// Internal use only
+int get_data_block_from_cache(big_int data_block_id, void *data_block);
+int put_data_block_in_cache(big_int data_block_id, void *data_block);
+void free_data_block_entry(struct data_block_entry *data_block_entry);
 
 #endif
